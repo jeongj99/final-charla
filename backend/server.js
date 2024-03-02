b7d4e17de07a6dd6e095fd49ac9f317f34b9e296
Jae Yun Jeong <jeongalex0616@gmail.com>
2024-03-02 16:07:32 -0500

    Router file

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8000;
 
 // Root route: http://localhost:8000/
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/api/auth/signup", (req, res) => {
-  console.log("Signup route");
-});
-
-app.get("/api/auth/login", (req, res) => {
-  console.log("Login route");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-  console.log("Logout route");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
