18177eeabbaacc1ace51e431fc791ea41f2ecfc6
Jae Yun Jeong <jeongalex0616@gmail.com>
2024-03-02 14:26:59 -0500

    Install nodemon and create test route

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// Root route: http://localhost:8000/
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(8000, () => console.log("Server running on port 8000"));
