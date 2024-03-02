25ac93eeb008351441b790fb269790cbf1817cd6
Jae Yun Jeong <jeongalex0616@gmail.com>
2024-03-02 14:41:06 -0500

    Import packages,create env and test routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
 // Root route: http://localhost:8000/
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!");
 });
 
-app.listen(8000, () => console.log("Server running on port 8000"));
+app.get("/api/auth/signup", (req, res) => {
+  console.log("Signup route");
+});
+
+app.get("/api/auth/login", (req, res) => {
+  console.log("Login route");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+  console.log("Logout route");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
