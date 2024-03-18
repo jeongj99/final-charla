ef38fa6acccb95c4be9994375ee7d952ef9f8b40
Jae Yun Jeong <jeongalex0616@gmail.com>
2024-03-17 23:02:12 -0400

    Finish first part of getting conversations for the sidebar and message container

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import useConversation from "../../store/useConversation";
 import { TiMessages } from "react-icons/ti";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
@@ -15,14 +17,19 @@ const NoChatSelected = () => {
 };
 
 const MessageContainer = () => {
-  const noChatSelected = false;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? <NoChatSelected /> : (
+      {!selectedConversation ? <NoChatSelected /> : (
         <>
           <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text">To: </span>
-            <span className="text-gray-900 font-bold">John Doe</span>
+            <span className="text-gray-900 font-bold">{selectedConversation.fullName}</span>
           </div>
           <Messages />
           <MessageInput />
