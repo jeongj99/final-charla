7a7915c4efd55026cf60095dabcdc115b4b86513
Jae Yun Jeong <jeongalex0616@gmail.com>
2024-03-18 23:13:52 -0400

    Implement get messages for selected conversation

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -8,18 +8,17 @@ const MessageInput = () => {
 
   const handleSubmit = async e => {
     e.preventDefault();
-
     if (!message) return;
     await sendMessage(message);
     setMessage("");
   };
 
   return (
-    <form className="px-4 my-3" onSubmit={handleSubmit}>
-      <div className="w-full relative">
+    <form className="my-3" onSubmit={handleSubmit}>
+      <div className="w-full relative flex">
         <input
           type="text"
-          className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
+          className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white ml-5 mr-9"
           placeholder="Send a message"
           value={message}
           onChange={e => setMessage(e.target.value)}
