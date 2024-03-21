aaa9b42e02428fd35fbf72d25256f0739790a2af
Jae Yun Jeong <jeongalex0616@gmail.com>
2024-03-20 23:31:43 -0400

    Implement socket.io

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,12 +3,14 @@ import useConversation from "../../store/useConversation";
 import { TiMessages } from "react-icons/ti";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
+import { useAuthContext } from "../../context/AuthContext";
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 John Doe ⚽️</p>
+        <p>Welcome 👋 {authUser.fullName}</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
